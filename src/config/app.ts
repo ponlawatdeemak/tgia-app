ad6e330502fc291f12d1c9982030d7cffb993c39
Warutniy <warut.n@mapboss.co.th>
2024-07-11 18:23:30 +0700

    update app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,12 +2,71 @@ export enum AppPath {
 	FieldLoss = '/field-loss',
 	PlotMonitoring = '/plot-monitoring',
 	AnnualAnalysis = '/annual-analysis',
+	Others = '/others',
+	Report = '/others/report',
+	UserManagement = '/others/user-management',
+	About = '/others/about',
+	Glossary = '/others/glossary',
+	Profile = '/profile',
 }
+
+export enum AreaType {
+	Registration = 'ทบก',
+	Insurance = 'เอาประกัน',
+}
+
+export enum AreaUnit {
+	Rai = 'ไร่',
+	LandPlot = 'แปลง',
+}
+
+export enum Language {
+	EN = 'en',
+	TH = 'th',
+}
+
+export const profileMenuConfig: {
+	key: keyof typeof AppPath
+	name: string
+	path: string
+} = {
+	key: 'Profile',
+	name: 'profile',
+	path: AppPath.Profile,
+}
+
+export const othersMenuConfig: {
+	key: keyof typeof AppPath
+	name: string
+	path: string
+}[] = [
+	{
+		key: 'Report',
+		name: 'รายงาน',
+		path: AppPath.Report,
+	},
+	{
+		key: 'UserManagement',
+		name: 'จัดการผู้ใช้งาน',
+		path: AppPath.UserManagement,
+	},
+	{
+		key: 'About',
+		name: 'เกี่ยวกับ',
+		path: AppPath.About,
+	},
+	{
+		key: 'Glossary',
+		name: 'อภิธานศัพท์',
+		path: AppPath.Glossary,
+	},
+]
 
 export const appMenuConfig: {
 	key: keyof typeof AppPath
 	name: string
 	path: string
+	children?: typeof othersMenuConfig
 }[] = [
 	{
 		key: 'FieldLoss',
@@ -23,5 +82,11 @@ export const appMenuConfig: {
 		key: 'AnnualAnalysis',
 		name: 'วิเคราะห์สถิติรายปี',
 		path: AppPath.AnnualAnalysis,
+	},
+	{
+		key: 'Others',
+		name: 'อื่นๆ',
+		path: AppPath.Others,
+		children: othersMenuConfig,
 	},
 ]
