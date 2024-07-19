8732f2f247d603f64701d254f0a2a2d27f78e574
Warutniy <warut.n@mapboss.co.th>
2024-07-19 09:42:33 +0700

    edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,15 @@ const nextConfig = {
 	serverRuntimeConfig: {
 		apiUrl: process.env.API_URL,
 		apiKey: process.env.API_KEY,
-	}
+	},
+	env: {
+        API_URL: process.env.API_URL,
+        API_KEY: process.env.API_KEY,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        APP_USERNAME: process.env.APP_USERNAME || '',
+        APP_PASSWORD: process.env.APP_PASSWORD || '',
+    },
 }
 
 export default nextConfig
