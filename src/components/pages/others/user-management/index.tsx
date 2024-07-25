961412379822090a68dfa2f9088aeef5ce408f7e
Warutniy <warut.n@mapboss.co.th>
2024-07-25 14:15:11 +0700

    fix: update user-management page

--- src/components/pages/others/user-management/index.tsx
+++ src/components/pages/others/user-management/index.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@mui/material'
+import UserManagementSearchForm from './components/SearchForm'
+import UserManagementTable from './components/Table'
 
 const UserManagementMain = () => {
 	return (
-		<>
-			<div>UserManagementMain</div>
-			<Button variant='contained'>Contained</Button>
-			<Button variant='outlined'>Outlined</Button>
-		</>
+		<div className='flex flex-col'>
+			<UserManagementSearchForm />
+			<UserManagementTable />
+		</div>
 	)
 }
 
