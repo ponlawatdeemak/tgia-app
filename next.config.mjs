e5423ee6d7e8dc81edf14f944e515846cf8f441e
Ponlawat Deemak <ponlawat.deemak@gmail.com>
2024-07-30 12:26:45 +0700

    test: add apiDisaster

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
 	env: {
 		API_URL: process.env.API_URL,
 		API_KEY: process.env.API_KEY,
+		API_URL_DISASTER: process.env.API_URL_DISASTER,
+		API_KEY_DISASTER: process.env.API_KEY_DISASTER,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		APP_USERNAME: process.env.APP_USERNAME || '',
--- src/api/dto/auth/index.ts
+++ src/api/dto/auth/index.ts
@@ -1,4 +1,5 @@
 import { api } from '@/api/core'
+import { apiDisaster } from '@/api/core-disaster'
 import {
 	ChangePasswordDtoIn,
 	ForgotPasswordDtoIn,
