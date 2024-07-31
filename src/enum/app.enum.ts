a7b28446b7956946998517e516e4316c573bf364
Warutniy <warut.n@mapboss.co.th>
2024-07-31 15:52:30 +0700

    fix: add app.enum

--- src/enum/app.enum.ts
+++ src/enum/app.enum.ts
@@ -4,11 +4,54 @@ export enum AreaTypeKey {
 }
 
 export enum AreaUnitKey {
-	Rai = 'rai',
-	LandPlot = 'plot',
+	Rai = 'areaRai',
+	LandPlot = 'areaPlot',
+}
+
+export enum AreaUnitText {
+	areaRai = 'areaRai',
+	areaPlot = 'areaPlot',
 }
 
 export enum Language {
 	EN = 'en',
 	TH = 'th',
+}
+
+export enum SortType {
+	ASC = 'asc',
+	DES = 'des',
+}
+
+export enum LossType {
+	NoData = 0,
+	Flood = 1,
+	Drought = 2,
+}
+
+export enum InsuredType {
+	NoInsurance = 0,
+	BasicInsurance = 1,
+	VoluntaryInsurance = 2,
+}
+
+export enum PublicType {
+	NoPublic = 0,
+	Public = 1,
+}
+
+export enum RiskType {
+	Low = 'low',
+	Medium = 'medium',
+	High = 'high',
+}
+
+export enum DetailType {
+	PhotoperiodSensitiveRice = 1,
+	NonPhotoperiodSensitiveRice = 2,
+}
+
+export enum BreedType {
+	JasmineRice = 1,
+	GlutinousRice = 2,
 }
