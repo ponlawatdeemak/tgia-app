c3faac5a79a273b2f35040230b86af6bf686cfa7
Suthika Wongsiridech <suthika.w@mapboss.co.th>
2024-08-06 13:55:59 +0700

    fix stuck in loading screen

--- src/components/providers/TokenProvider.tsx
+++ src/components/providers/TokenProvider.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { apiAccessToken, updateAccessToken } from '@/api/core'
+import { updateAccessToken } from '@/api/core'
 import TokenExpiredDialog from '@/components/TokenExpiredDialog'
 import LoadingScreen from '@/components/common/loading/LoadingScreen'
 import { authPathPrefix } from '@/config/app'
-import { signIn, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { PropsWithChildren, useEffect, useMemo } from 'react'
+import { PropsWithChildren, useEffect, useMemo, useState } from 'react'
 
 interface TokenProviderProps extends PropsWithChildren {}
 
@@ -14,21 +14,17 @@ const TokenProvider: React.FC<TokenProviderProps> = ({ children }) => {
 	const pathname = usePathname()
 	const searchParams = useSearchParams()
 	const { data: session } = useSession()
+	const [accessToken, setAccessToken] = useState<string | null>(null)
 
 	const sessionExpired = useMemo(() => Number(searchParams?.get('sessionExpired') ?? 0), [searchParams])
 
 	useEffect(() => {
-		if (session?.error === 'RefreshAccessTokenError') {
-			signIn()
-		}
 		const userId = session?.user?.id
 		const accessToken = session?.user?.tokens?.accessToken
 		const refreshToken = session?.user?.tokens?.refreshToken
-		// console.log('------- token provider -------')
-		// console.log('TLOG ~ accessToken:', accessToken)
-		// console.log('TLOG ~ refreshToken:', refreshToken)
 		if (accessToken) {
 			updateAccessToken({ accessToken, refreshToken, userId })
+			setAccessToken(accessToken)
 		} else {
 			updateAccessToken({})
 		}
@@ -47,7 +43,7 @@ const TokenProvider: React.FC<TokenProviderProps> = ({ children }) => {
 		)
 	}
 
-	if (apiAccessToken) {
+	if (accessToken) {
 		return children
 	}
 
