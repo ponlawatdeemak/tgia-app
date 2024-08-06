62d35c4dd3716f596b5819006034df8eb13ccf7d
Suthika Wongsiridech <suthika.w@mapboss.co.th>
2024-08-06 17:06:38 +0700

    fix layout

--- src/components/pages/field-loss/Main/index.tsx
+++ src/components/pages/field-loss/Main/index.tsx
@@ -9,9 +9,9 @@ export const FieldLossMain = () => {
 	return (
 		<div className='flex flex-grow flex-col gap-y-[16px]'>
 			<SearchForm />
-			<Paper className='flex h-full overflow-hidden rounded-none px-4 lg:mx-4 lg:rounded-lg'>
-				{/* <FieldLossSummary /> */}
-				<div className='relative'>
+			<Paper className='flex h-full overflow-hidden rounded-none px-4 lg:mx-4 lg:mb-4 lg:rounded-lg lg:px-0'>
+				<FieldLossSummary />
+				<div className='flex w-full'>
 					<MapView />
 				</div>
 			</Paper>
