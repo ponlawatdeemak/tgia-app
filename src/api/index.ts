bb81255672ed7393f556a1d4551472b027db87e5
Suthika Wongsiridech <suthika.w@mapboss.co.th>
2024-08-06 12:32:54 +0700

    add disaster api call

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,16 @@
 import auth from '@/api/auth'
-import um from '@/api/um'
-import lookup from '@/api/lookup'
 import fieldLoss from '@/api/field-loss'
+import lookup from '@/api/lookup'
+import um from '@/api/um'
+import { api } from './core'
+import { APIService } from './interface'
 
 const service = {
 	auth,
 	um,
 	lookup,
 	fieldLoss,
+	example: { getVersion: async (): Promise<any> => await api.get('/version', APIService.DisasterAPI) },
 }
 
 export default service
