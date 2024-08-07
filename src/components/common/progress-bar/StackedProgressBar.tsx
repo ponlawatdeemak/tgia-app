3f00e634625ecbbe0f1c0caca4638cb1be31880b
Warutniy <warut.n@mapboss.co.th>
2024-08-07 18:05:32 +0700

    fix: update StackedProgressBar

--- src/components/common/progress-bar/StackedProgressBar.tsx
+++ src/components/common/progress-bar/StackedProgressBar.tsx
@@ -8,18 +8,19 @@ export interface StackedProgressBarData {
 
 export interface StackedProgressBarProps {
 	data: StackedProgressBarData[]
+	percentTotal?: number
 }
 
-const StackedProgressBar: React.FC<StackedProgressBarProps> = ({ data }) => {
+const StackedProgressBar: React.FC<StackedProgressBarProps> = ({ data, percentTotal }) => {
 	return (
 		<div>
-			<div className='h-4' style={{}}>
+			<div className='flex h-4 gap-0.5' style={{ width: `${percentTotal}%` }}>
 				{/* <div style={{ borderTop: '2px dotted #D6D6D6' }}></div> */}
 				{data.map((item, index) => {
 					return (
 						<div
 							key={index}
-							className={`float-left bg-[${item.color}] mr-0.5 h-full`}
+							className={`float-left bg-[${item.color}] h-full`}
 							style={{
 								width: `${item.percent}%`,
 								backgroundColor: `${item.color}`,
