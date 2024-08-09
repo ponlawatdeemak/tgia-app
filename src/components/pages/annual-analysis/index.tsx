06fc85c73ae26fd56c46cb68233f1c819f0d1208
Suthika Wongsiridech <suthika.w@mapboss.co.th>
2024-08-09 18:04:04 +0700

    create horizontal date picker

--- src/components/pages/annual-analysis/index.tsx
+++ src/components/pages/annual-analysis/index.tsx
@@ -1,17 +1,24 @@
 import service from '@/api'
+import DatePickerHorizontal from '@/components/shared/DatePickerHorizontal'
 import { Button } from '@mui/material'
+import { addDays, addYears } from 'date-fns'
 
 const AnnualAnalysisMain = async () => {
-	try {
-		const data = await service.example.getVersion()
-	} catch (error) {
-		// console.log(error)
-	}
+	// try {
+	// 	const data = await service.example.getVersion()
+	// } catch (error) {
+	// 	// console.log(error)
+	// }
 	return (
 		<>
-			<div>AnnualAnalysisMain</div>
+			{/* <div>AnnualAnalysisMain</div>
 			<Button variant='contained'>Contained</Button>
-			<Button variant='outlined'>Outlined</Button>
+			<Button variant='outlined'>Outlined</Button> */}
+			<div className='flex justify-center'>
+				<div className='w-[800px]'>
+					<DatePickerHorizontal startDate={new Date()} endDate={addDays(new Date(), 36)} />
+				</div>
+			</div>
 		</>
 	)
 }
