b9c649e019c41dc5506315a09c3e71336b504119
Warutniy <warut.n@mapboss.co.th>
2024-08-13 14:50:39 +0700

    fix: create calculation dialog

--- src/components/pages/field-loss/Main/Summary.tsx
+++ src/components/pages/field-loss/Main/Summary.tsx
@@ -1,4 +1,16 @@
-import { Box, Card, CardContent, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
+import {
+	Box,
+	Card,
+	CardContent,
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	IconButton,
+	Paper,
+	ToggleButton,
+	ToggleButtonGroup,
+	Typography,
+} from '@mui/material'
 import React, { useState } from 'react'
 import clsx from 'clsx'
 import { AreaTypeKey, AreaUnitKey, Language } from '@/enum'
@@ -10,6 +22,10 @@ import { useTranslation } from 'react-i18next'
 import dayjs, { Dayjs } from 'dayjs'
 import { useQuery } from '@tanstack/react-query'
 import service from '@/api'
+import RegistrationCalculation from '@/components/svg/field-loss/RegistrationCalculation'
+import { mdiClose } from '@mdi/js'
+import Icon from '@mdi/react'
+import InsuranceCalculation from '@/components/svg/field-loss/InsuranceCalculation'
 
 interface OptionType {
 	name: string
@@ -43,6 +59,7 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 	const { areaUnit } = useAreaUnit()
 	const { t, i18n } = useTranslation(['default', 'field-loss'])
 	const [selectedCard, setSelecteeCard] = useState<number>(2)
+	const [openCalDialog, setOpenCalDialog] = useState<boolean>(false)
 
 	const { data: summaryData, isLoading: isSummaryDataLoading } = useQuery({
 		queryKey: ['getSummaryPredictedLoss', lossType, startDate, endDate, areaType, selectedOption?.id],
@@ -56,13 +73,6 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 			}),
 	})
 
-	// console.log('lossType', lossType ?? undefined)
-	// console.log('startDate', startDate?.toISOString().split('T')[0])
-	// console.log('endDate', endDate?.toISOString().split('T')[0])
-	// console.log('areaType', areaType)
-	// console.log('provinceId', selectedOption?.id)
-	// console.log('summaryData', summaryData)
-
 	const handleLossTypeClick = (_event: React.MouseEvent<HTMLElement>, newAlignment: LossType | null) => {
 		setLossType(newAlignment)
 	}
@@ -71,14 +81,24 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 		setSelecteeCard(cardIndex)
 	}
 
+	const handleOpenCalDialog = (event: React.MouseEvent<HTMLElement>) => {
+		event.stopPropagation()
+		setOpenCalDialog(true)
+	}
+
+	const handleCloseCalDialog = (event: React.MouseEvent<HTMLElement>) => {
+		event.stopPropagation()
+		setOpenCalDialog(false)
+	}
+
 	return (
-		<div className='lg:bg-gray-light2 box-border flex flex-col gap-0 bg-gray-light p-0 lg:w-[30%] lg:min-w-[360px] lg:max-w-[580px] lg:gap-4 lg:px-[22px] lg:py-4'>
+		<div className='box-border flex flex-col gap-0 bg-gray-light p-0 lg:w-[30%] lg:min-w-[360px] lg:max-w-[580px] lg:gap-4 lg:bg-gray-light2 lg:px-[22px] lg:py-4'>
 			<ToggleButtonGroup
 				value={lossType}
 				exclusive
 				onChange={handleLossTypeClick}
 				aria-label='loss-type'
-				className='lg:border-gray-light2 flex gap-2 border-2 border-solid border-gray-light max-lg:py-3 lg:gap-1 [&_*]:rounded [&_*]:border-none [&_*]:px-3 [&_*]:py-1.5'
+				className='flex gap-2 border-2 border-solid border-gray-light max-lg:py-3 lg:gap-1 lg:border-gray-light2 [&_*]:rounded [&_*]:border-none [&_*]:px-3 [&_*]:py-1.5'
 			>
 				<ToggleButton
 					className={clsx('text-base', {
@@ -109,18 +129,18 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 				</ToggleButton>
 			</ToggleButtonGroup>
 			<Box className='flex flex-col gap-3 lg:gap-2'>
-				<Card className='bg-gray-dark3 lg:border-gray-light2 box-border w-full border-2 border-solid border-gray-light px-4 py-3 max-lg:rounded'>
+				<Card className='box-border w-full border-2 border-solid border-gray-light bg-gray-dark3 px-4 py-3 max-lg:rounded lg:border-gray-light2'>
 					<CardContent className='flex flex-col gap-3 p-0'>
 						<Typography variant='body1' className='text-left text-md font-semibold text-black-dark'>
 							{t('allRegisteredAreas', { ns: 'field-loss' })}
 						</Typography>
 						<div className='flex items-baseline justify-end gap-1'>
-							<span className='text-black-light text-xl font-semibold'>
+							<span className='text-xl font-semibold text-black-light'>
 								{summaryData?.data?.actAreaNoGeom[areaUnit].toLocaleString()}
 							</span>
 							<span className='text-base text-black-dark'>{t(areaUnit)}</span>
 						</div>
-						<span className='text-gray-dark2 text-left text-sm font-medium'>
+						<span className='text-left text-sm font-medium text-gray-dark2'>
 							{t('lastUpdated', { ns: 'field-loss' })} 24 มี.ค. 2568
 						</span>
 					</CardContent>
@@ -136,7 +156,7 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 						value={1}
 						className={clsx('m-0 p-0 max-lg:rounded', {
 							'border-2 border-primary': selectedCard === 1,
-							'hover:border-gray-light2 border-2 border-transparent': selectedCard !== 1,
+							'border-2 border-transparent hover:border-gray-light2': selectedCard !== 1,
 						})}
 					>
 						<Card
@@ -164,9 +184,59 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 										{t('percentTotalRegisteredAreas', { ns: 'field-loss' })}
 									</p>
 								</div>
-								<span className='text-right text-sm font-normal text-black-dark underline'>
+								<span
+									onClick={(event) => handleOpenCalDialog(event)}
+									className='text-right text-sm font-normal text-black-dark underline'
+								>
 									{t('calculationMethod', { ns: 'field-loss' })}
 								</span>
+								<Dialog
+									open={openCalDialog}
+									onClick={(event) => handleCloseCalDialog(event)}
+									aria-labelledby='alert-dialog-title'
+									aria-describedby='alert-dialog-description'
+									PaperComponent={({ children }) => (
+										<Paper
+											className={clsx('m-0 flex flex-col gap-4 p-6 max-lg:mx-4', {
+												'lg:pl-4': areaType === AreaTypeKey.Insurance,
+											})}
+										>
+											{children}
+										</Paper>
+									)}
+								>
+									<DialogTitle
+										className='flex items-center justify-between p-0'
+										id='alert-dialog-title'
+									>
+										<Typography
+											className={clsx('text-sm font-semibold text-black-dark lg:text-lg', {
+												'lg:ml-2': areaType === AreaTypeKey.Insurance,
+											})}
+										>
+											{areaType === AreaTypeKey.Registration
+												? 'วิธีการคำนวนพื้นที่ประมาณการการเยียวยา'
+												: 'วิธีการคำนวนพื้นที่ประมาณการเคลมประกัน'}
+										</Typography>
+										<IconButton aria-label='close' onClick={(event) => handleCloseCalDialog(event)}>
+											<Icon
+												path={mdiClose}
+												className='h-4 w-4 font-light text-black lg:h-6 lg:w-6'
+											/>
+										</IconButton>
+									</DialogTitle>
+									<DialogContent className='p-0 max-lg:[&_svg]:!h-full max-lg:[&_svg]:!w-full'>
+										{areaType === AreaTypeKey.Registration ? (
+											<Box className='overflow-hidden rounded-lg lg:h-[94px] lg:w-[433px]'>
+												<RegistrationCalculation width={433} height={94} />
+											</Box>
+										) : (
+											<Box className='h-[38px] overflow-hidden rounded-lg lg:h-[94px] lg:w-[799px]'>
+												<InsuranceCalculation width={799} height={94} />
+											</Box>
+										)}
+									</DialogContent>
+								</Dialog>
 							</CardContent>
 						</Card>
 					</ToggleButton>
@@ -174,7 +244,7 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 						value={2}
 						className={clsx('m-0 p-0 max-lg:rounded', {
 							'border-2 border-primary': selectedCard === 2,
-							'hover:border-gray-light2 border-2 border-transparent': selectedCard !== 2,
+							'border-2 border-transparent hover:border-gray-light2': selectedCard !== 2,
 						})}
 					>
 						<Card
@@ -202,7 +272,7 @@ const FieldLossSummary: React.FC<FieldLossSummaryProps> = ({
 										/>
 									))}
 								</div>
-								<span className='text-gray-dark2 text-left text-sm font-medium'>
+								<span className='text-left text-sm font-medium text-gray-dark2'>
 									{t('latestDataAnalysis', { ns: 'field-loss' })} 24 มี.ค. 2568
 								</span>
 							</CardContent>
