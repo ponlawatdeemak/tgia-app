e84458283102b61a97f7b295b175ad5b37fbc914
Chokchai Bisgood <chokchai.b@mapboss.co.th>
2024-08-16 09:25:10 +0700

    fix: fix bug add profile, change useMutation

--- src/components/pages/others/user-management/Form/index.tsx
+++ src/components/pages/others/user-management/Form/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Dialog, DialogActions, DialogContent, DialogTitle, Snackbar } from '@mui/material'
+import { Alert, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Snackbar } from '@mui/material'
 import React, { FormEvent, useState, useCallback, useEffect } from 'react'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import IOSSwitch from '@/components/common/switch/IOSSwitch'
@@ -6,7 +6,7 @@ import ProfileForm from '@/components/shared/ProfileForm'
 import { AlertInfoType, FormValues } from '@/components/shared/ProfileForm/interface'
 import { useFormik } from 'formik'
 import service from '@/api'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import * as yup from 'yup'
 import { useTranslation } from 'react-i18next'
 import { useSwitchLanguage } from '@/i18n/client'
@@ -15,6 +15,10 @@ import { useSession } from 'next-auth/react'
 import AlertConfirm from '@/components/common/dialog/AlertConfirm'
 import { DeleteProfileDtoIn, PostProfileUMDtoIn, PostUploadFilesDtoIn, PutProfileUMDtoIn } from '@/api/um/dto-in.dto'
 import um from '@/api/um'
+import Icon from '@mdi/react'
+import { mdiTrashCanOutline } from '@mdi/js'
+import useResponsive from '@/hook/responsive'
+import classNames from 'classnames'
 
 export interface UserManagementProps {
 	open: boolean
@@ -41,13 +45,12 @@ const defaultFormValues: UMFormValues = {
 	role: '',
 	responsibleProvinceCode: '',
 	responsibleDistrictCode: '',
-	flagStatus: '',
+	flagStatus: 'C',
 }
 
 export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 	const { t, i18n } = useTranslation(['default', 'um'])
 	const { i18n: i18nWithCookie } = useSwitchLanguage(i18n.language as Language, 'appbar')
-	// const [isUserActive, setIsUserActive] = useState<boolean>(false)
 	const [isConfirmAddOpen, setIsConfirmAddOpen] = useState<boolean>(false)
 	const [isConfirmEditOpen, setIsConfirmEditOpen] = useState<boolean>(false)
 	const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState<boolean>(false)
@@ -57,6 +60,7 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 		message: '',
 	})
 	const { data: session } = useSession()
+	const { isDesktop } = useResponsive()
 	const handleSubmitUser = async (event: FormEvent) => {
 		event.preventDefault()
 		props.isEdit ? setIsConfirmEditOpen(true) : setIsConfirmAddOpen(true)
@@ -75,10 +79,32 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 			const res = await service.um.getUM({
 				userId: props.userId,
 			})
-			console.log(res.data)
+			// console.log(res.data)
 			return res
 		},
 		enabled: !!props.userId,
+	})
+
+	const {
+		data: putProfileUMData,
+		error: putProfileUMError,
+		mutateAsync: mutatePutProfileUM,
+		isPending: isPutProfileUMPending,
+	} = useMutation({
+		mutationFn: async (payload: PutProfileUMDtoIn) => {
+			return await um.putProfileUM(payload)
+		},
+	})
+
+	const {
+		data: postProfileUMData,
+		error: postProfileUMError,
+		mutateAsync: mutatePostProfileUM,
+		isPending: isPostProfileUMPending,
+	} = useMutation({
+		mutationFn: async (payload: PostProfileUMDtoIn) => {
+			return await um.postProfileUM(payload)
+		},
 	})
 
 	const onDelete = useCallback(
@@ -92,6 +118,7 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 				const res = await um.deleteProfile(payload)
 				props.setIsSearch(true)
 				setAlertInfo({ open: true, severity: 'success', message: t('profileDelete', { ns: 'um' }) })
+				props.setOpen(false)
 			} catch (error: any) {
 				setAlertInfo({
 					open: true,
@@ -103,126 +130,133 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 		[props, session?.user.id, t],
 	)
 
-	const onSubmit = useCallback(async (values: UMFormValues) => {
-		try {
+	const onSubmit = useCallback(
+		async (values: UMFormValues) => {
 			try {
-				// images is newly added
-				let imageUrl
-				if (values.image instanceof File) {
-					// New image uploaded
-					const imagePayload: PostUploadFilesDtoIn = {
-						file: values.image,
+				try {
+					// images is newly added
+					let imageUrl
+					if (values.image instanceof File) {
+						// New image uploaded
+						const imagePayload: PostUploadFilesDtoIn = {
+							file: values.image,
+						}
+						const res = await um.postUploadFiles(imagePayload)
+						imageUrl = res.data?.download_file_url
 					}
-					const res = await um.postUploadFiles(imagePayload)
-					imageUrl = res.data?.download_file_url
-				}
-				if (imageUrl) {
-					console.log('new image uploaded')
-					values.image = imageUrl
-				} else {
-					console.log('no image uploaded or already existing image')
-					values.image = ""
-				}
-				if (props.isEdit) {
-					// put method edit existing user
-					const payload: PutProfileUMDtoIn = {
-						id: values.id,
-						username: values.username,
-						firstName: values.firstName,
-						lastName: values.lastName,
-						email: values.email,
-						image: values.image,
-						orgCode: values.orgCode,
-						role: values.role,
-						responsibleProvinceCode: values.responsibleProvinceCode,
-						responsibleDistrictCode: values.responsibleDistrictCode,
-						flagStatus: values.flagStatus,
+					if (imageUrl) {
+						console.log('new image uploaded')
+						values.image = imageUrl
+					} else {
+						console.log('no image uploaded or already existing image')
+						values.image = ''
 					}
-					console.log("Editing user :: ",payload)
-					const res = await um.putProfileUM(payload)
-					console.log("res :: ",res)
-					props.setIsSearch(true)
-				} else {
-					// post method add new user
-					const payload: PostProfileUMDtoIn = {
-						username: values.username,
-						firstName: values.firstName,
-						lastName: values.lastName,
-						email: values.email,
-						image: values.image,
-						orgCode: values.orgCode,
-						role: values.role,
-						responsibleProvinceCode: values.responsibleProvinceCode,
-						responsibleDistrictCode: values.responsibleDistrictCode,
-						flagStatus: values.flagStatus,
+					if (props.isEdit) {
+						// put method edit existing user
+						const payload: PutProfileUMDtoIn = {
+							id: values.id,
+							username: values.username,
+							firstName: values.firstName,
+							lastName: values.lastName,
+							email: values.email,
+							image: values.image,
+							orgCode: values.orgCode,
+							role: values.role,
+							responsibleProvinceCode: values.responsibleProvinceCode,
+							responsibleDistrictCode: values.responsibleDistrictCode,
+							flagStatus: values.flagStatus,
+						}
+						console.log('Editing user :: ', payload)
+						const res = await mutatePutProfileUM(payload)
+						setAlertInfo({ open: true, severity: 'success', message: t('profileUpdate', { ns: 'um' }) })
+						console.log('res :: ', res)
+						props.setIsSearch(true)
+					} else {
+						// post method add new user
+						const payload: PostProfileUMDtoIn = {
+							username: values.username,
+							firstName: values.firstName,
+							lastName: values.lastName,
+							email: values.email,
+							image: values.image,
+							orgCode: values.orgCode,
+							role: values.role,
+							responsibleProvinceCode: values.responsibleProvinceCode,
+							responsibleDistrictCode: values.responsibleDistrictCode,
+							flagStatus: values.flagStatus,
+						}
+						console.log('Adding new user :: ', payload)
+						const res = await mutatePostProfileUM(payload)
+						setAlertInfo({ open: true, severity: 'success', message: t('profileAdd', { ns: 'um' }) })
+						console.log('res :: ', res)
+						props.setIsSearch(true)
+						props.setOpen(false)
+						formik.resetForm()
 					}
-					console.log("Adding new user :: ", payload)
-					const res = await um.postProfileUM(payload)
-					console.log("res :: ",res)
-					props.setIsSearch(true)
+				} catch (error: any) {
+					console.log(error)
+					setAlertInfo({
+						open: true,
+						severity: 'error',
+						message: error?.title ? error.title : t('error.somethingWrong'),
+					})
 				}
-			} catch (error) {
-				console.log(error)
-				setAlertInfo({
-					open: true,
-					severity: 'error',
-					message: t('error.somethingWrong'),
-				})
+				// setBusy(true)
+				// if (values.image instanceof File) {
+				// 	const selectedImage: CreateProfileImageDtoIn = {
+				// 		file: values.image,
+				// 	}
+				// 	let imageUrl
+				// 	try {
+				// 		imageUrl = await service.um.uploadImg(selectedImage)
+				// 	} catch (error) {
+				// 		throw new Error('Image upload failed')
+				// 	}
+				// 	values.image = imageUrl.data?.download_file_url || ''
+				// }
+				// const profileData: PutProfileDtoIn = {
+				// 	id: values.id,
+				// 	firstName: values.firstName,
+				// 	lastName: values.lastName,
+				// 	email: values.email,
+				// 	image: values.image,
+				// 	responsibleProvinceCode: values.responsibleProvinceCode,
+				// 	responsibleDistrictCode: values.responsibleDistrictCode,
+				// }
+				// try {
+				// 	await mutateUpdateProfile(profileData)
+				// } catch (error) {
+				// 	throw new Error('Profile update failed')
+				// }
+				// let userImage
+				// try {
+				// 	userImage = (await service.um.getProfile()).data?.image
+				// } catch (error) {
+				// 	throw new Error('Access Profile failed')
+				// }
+				// // ใช้ update ค่า data จาก useSession
+				// try {
+				// 	await update({
+				// 		firstName: profileData.firstName,
+				// 		lastName: profileData.lastName,
+				// 		email: profileData.email,
+				// 		image: userImage,
+				// 		responsibleProvinceCode: profileData.responsibleProvinceCode,
+				// 		responsibleDistrictCode: profileData.responsibleDistrictCode,
+				// 	})
+				// } catch (error) {
+				// 	throw new Error('Failed to update session')
+				// }
+				// setAlertInfo({ open: true, severity: 'success', message: 'แก้ไขข้อมูลส่วนตัวสำเร็จ' })
+			} catch (error: any) {
+				// console.log('Error:', error.message)
+				// setAlertInfo({ open: true, severity: 'error', message: 'แก้ไขข้อมูลส่วนตัวไม่สำเร็จ' })
+			} finally {
+				// setBusy(false)
 			}
-			// setBusy(true)
-			// if (values.image instanceof File) {
-			// 	const selectedImage: CreateProfileImageDtoIn = {
-			// 		file: values.image,
-			// 	}
-			// 	let imageUrl
-			// 	try {
-			// 		imageUrl = await service.um.uploadImg(selectedImage)
-			// 	} catch (error) {
-			// 		throw new Error('Image upload failed')
-			// 	}
-			// 	values.image = imageUrl.data?.download_file_url || ''
-			// }
-			// const profileData: PutProfileDtoIn = {
-			// 	id: values.id,
-			// 	firstName: values.firstName,
-			// 	lastName: values.lastName,
-			// 	email: values.email,
-			// 	image: values.image,
-			// 	responsibleProvinceCode: values.responsibleProvinceCode,
-			// 	responsibleDistrictCode: values.responsibleDistrictCode,
-			// }
-			// try {
-			// 	await mutateUpdateProfile(profileData)
-			// } catch (error) {
-			// 	throw new Error('Profile update failed')
-			// }
-			// let userImage
-			// try {
-			// 	userImage = (await service.um.getProfile()).data?.image
-			// } catch (error) {
-			// 	throw new Error('Access Profile failed')
-			// }
-			// // ใช้ update ค่า data จาก useSession
-			// try {
-			// 	await update({
-			// 		firstName: profileData.firstName,
-			// 		lastName: profileData.lastName,
-			// 		email: profileData.email,
-			// 		image: userImage,
-			// 		responsibleProvinceCode: profileData.responsibleProvinceCode,
-			// 		responsibleDistrictCode: profileData.responsibleDistrictCode,
-			// 	})
-			// } catch (error) {
-			// 	throw new Error('Failed to update session')
-			// }
-			// setAlertInfo({ open: true, severity: 'success', message: 'แก้ไขข้อมูลส่วนตัวสำเร็จ' })
-		} catch (error: any) {
-			// console.log('Error:', error.message)
-			// setAlertInfo({ open: true, severity: 'error', message: 'แก้ไขข้อมูลส่วนตัวไม่สำเร็จ' })
-		} finally {
-			// setBusy(false)
-		}
-	}, [])
+		},
+		[mutatePostProfileUM, mutatePutProfileUM, props, t],
+	)
 
 	// const logout = useCallback(() => signOut({ callbackUrl: AppPath.Login }), [])
 
@@ -247,7 +281,17 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 				</DialogTitle>
 				<DialogContent className='h-[492px]' dividers={true}>
 					{/* override class from parent */}
-					<ProfileForm formik={formik}></ProfileForm>
+					{/* <Box sx={{
+						'.MuiBox-root' : {
+							display: 'flex',
+							flexDirection: 'column'
+						},
+					}}> */}
+					<ProfileForm
+						formik={formik}
+						loading={isPostProfileUMPending || isPutProfileUMPending || isUserDataLoading}
+					></ProfileForm>
+					{/* </Box> */}
 					{session?.user.id !== props.userId && (
 						<FormControlLabel
 							control={
@@ -263,24 +307,37 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 						/>
 					)}
 				</DialogContent>
-				<DialogActions className='p-6'>
-					{session?.user.id !== props.userId && (
+				<DialogActions className='flex justify-between p-6'>
+					{session?.user.id !== props.userId && props.isEdit && (
 						<Button
-							className='text-red bg-white text-sm text-[#D13438]'
+							className='text-red h-[40px] w-[124px] bg-white text-sm text-error'
 							variant='contained'
 							onClick={() => {
 								setIsConfirmDeleteOpen(true)
 							}}
+							startIcon={<Icon path={mdiTrashCanOutline} size={1} color={'var(--error-color-1)'} />}
 						>
-							ลบผู้ใช้งาน
+							{t('deleteUser', { ns: 'um' })}
 						</Button>
 					)}
-					<Button className='bg-white text-sm text-black' variant='contained' onClick={props.onClose}>
-						ยกเลิก
-					</Button>
-					<Button type='submit' variant='contained' color='primary'>
-						บันทึก
-					</Button>
+					<div className='flex space-x-2'>
+						<Button
+							className='h-[40px] w-[71px] bg-white text-sm text-black'
+							variant='contained'
+							onClick={props.onClose}
+						>
+							{t('cancel')}
+						</Button>
+						<Button
+							type='submit'
+							variant='contained'
+							color='primary'
+							className='h-[40px] w-[71px] text-sm [&_.MuiButtonBase-root]:w-[71px]'
+						>
+							{/* {t('save')} */}
+							บันทึก
+						</Button>
+					</div>
 				</DialogActions>
 			</Dialog>
 			{/* Alert Confirm Add New */}
@@ -322,7 +379,6 @@ export const FormMain: React.FC<UserManagementProps> = ({ ...props }) => {
 				onConfirm={() => {
 					onDelete(props.userId)
 					setIsConfirmDeleteOpen(false)
-					props.setOpen(false)
 				}}
 			/>
 
