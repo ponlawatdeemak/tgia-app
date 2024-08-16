4715d08f7c4f6ff38c0670f2a673726306811e18
Warutniy <warut.n@mapboss.co.th>
2024-08-16 09:28:58 +0700

    fix: create screenshot file

---
+++ src/utils/screenshot.ts
@@ -0,0 +1,28 @@
+import html2canvas from 'html2canvas'
+
+export const onCapture = () => {
+	const timestamp = new Date().toISOString().replace(/[:.-]/g, '_')
+	const filename = `screenshot_${timestamp}.png`
+	html2canvas(document.body, {
+		useCORS: true,
+		allowTaint: true,
+		scrollX: 0,
+		scrollY: 0,
+		onclone: function (clone) {
+			clone.body.style.height = 'unset'
+			const elements = clone.getElementsByClassName('capture')
+			for (let index = 0; index < elements.length; index++) {
+				const element = elements[index] as HTMLElement
+				element.style.overflowY = 'visible !important'
+				element.style.maxHeight = 'unset !important'
+			}
+			return true
+		},
+	}).then((canvas) => {
+		const img = canvas.toDataURL('image/png')
+		const link = document.createElement('a')
+		link.href = img
+		link.download = filename
+		link.click()
+	})
+}
