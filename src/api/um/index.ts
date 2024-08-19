d533c1108065834a95ac37d5f933dd5aa168f8bd
Chokchai Bisgood <chokchai.b@mapboss.co.th>
2024-08-19 14:48:43 +0700

    fix: add post import um, get template um

--- src/api/um/index.ts
+++ src/api/um/index.ts
@@ -1,7 +1,38 @@
 import { api } from '@/api/core'
-import { DeleteProfileDtoOut, GetProfileDtoOut, GetSearchUMDtoOut, GetUmDtoOut, PatchStatusDtoOut, PostProfileUMDtoOut, PostUploadFilesDtoOut, PutProfileDtoOut, PutProfileUMDtoOut } from '@/api/um/dto-out.dto'
-import { DeleteProfileDtoIn, GetSearchUMDtoIn, GetUmDtoIn, PatchStatusDtoIn, PostProfileUMDtoIn, PostUploadFilesDtoIn, PutProfileDtoIn, PutProfileUMDtoIn } from '@/api/um/dto-in.dto'
+import {
+	DeleteProfileDtoOut,
+	GetProfileDtoOut,
+	GetSearchUMDtoOut,
+	GetUmDtoOut,
+	PatchStatusDtoOut,
+	PostProfileUMDtoOut,
+	PostUploadFilesDtoOut,
+	PutProfileDtoOut,
+	PutProfileUMDtoOut,
+} from '@/api/um/dto-out.dto'
+import {
+	DeleteProfileDtoIn,
+	GetSearchUMDtoIn,
+	GetUmDtoIn,
+	PatchStatusDtoIn,
+	PostImportCSVUMDtoIn,
+	PostImportXLSXUMDtoIn,
+	PostProfileUMDtoIn,
+	PostUploadFilesDtoIn,
+	PutProfileDtoIn,
+	PutProfileUMDtoIn,
+} from '@/api/um/dto-in.dto'
 import { APIService, ResponseDto } from '@/api/interface'
+import axios from 'axios'
+
+// define responseType of method to blob
+const instance = axios.create({
+	baseURL: process.env.API_URL,
+	headers: {
+		'x-api-key': process.env.API_KEY || '',
+	},
+	responseType: 'blob',
+})
 
 // Api for Profile and UM
 const um = {
@@ -18,14 +49,24 @@ const um = {
 	getProfile: async (): Promise<ResponseDto<GetProfileDtoOut>> => await api.get('/profile'),
 	putProfile: async (payload: PutProfileDtoIn): Promise<ResponseDto<PutProfileDtoOut>> =>
 		await api.put('/profile', payload),
-	getSearchUM: async (payload: GetSearchUMDtoIn): Promise<ResponseDto<GetSearchUMDtoOut[]>> => 
-		(await api.get(`/um/search?keyword=${payload.keyword}&sortField=${payload.sortField}&sortOrder=${payload.sortOrder}&limit=${payload.limit}&offset=${payload.offset}`)),
-	patchStatus: async (payload: PatchStatusDtoIn) : Promise<ResponseDto<PatchStatusDtoOut>> =>
-		await api.patch(`/um/${payload.id}`,payload),
-	deleteProfile: async (payload: DeleteProfileDtoIn) : Promise<ResponseDto<DeleteProfileDtoOut>> => await api.delete(`/um/${payload.id}`),
-	putProfileUM: async (payload: PutProfileUMDtoIn) : Promise<ResponseDto<PutProfileUMDtoOut>> => await api.put(`/um/${payload.id}`,payload),
-	postProfileUM: async (payload: PostProfileUMDtoIn): Promise<ResponseDto<PostProfileUMDtoOut>> => await api.post('/um',payload),
-	getTemplateCSVUM: async (): Promise<ResponseDto> => await api.get('/um/import/template/csv')
+	getSearchUM: async (payload: GetSearchUMDtoIn): Promise<ResponseDto<GetSearchUMDtoOut[]>> =>
+		await api.get(
+			`/um/search?keyword=${payload.keyword}&sortField=${payload.sortField}&sortOrder=${payload.sortOrder}&limit=${payload.limit}&offset=${payload.offset}`,
+		),
+	patchStatus: async (payload: PatchStatusDtoIn): Promise<ResponseDto<PatchStatusDtoOut>> =>
+		await api.patch(`/um/${payload.id}`, payload),
+	deleteProfile: async (payload: DeleteProfileDtoIn): Promise<ResponseDto<DeleteProfileDtoOut>> =>
+		await api.delete(`/um/${payload.id}`),
+	putProfileUM: async (payload: PutProfileUMDtoIn): Promise<ResponseDto<PutProfileUMDtoOut>> =>
+		await api.put(`/um/${payload.id}`, payload),
+	postProfileUM: async (payload: PostProfileUMDtoIn): Promise<ResponseDto<PostProfileUMDtoOut>> =>
+		await api.post('/um', payload),
+	getTemplateCSVUM: async (): Promise<ResponseDto> => await instance.get('/um/import/template/csv'),
+	getTemplateXLSXUM: async (): Promise<ResponseDto> => await instance.get('/um/import/template/xlsx'),
+	postImportCSVUM: async (payload: PostImportCSVUMDtoIn): Promise<ResponseDto> =>
+		await api.post('/um/import/csv', payload.data),
+	postImportXLSXUM: async (payload: PostImportXLSXUMDtoIn): Promise<ResponseDto> =>
+		await api.post('/um/import/xlsx', payload.data),
 }
 
 export default um
