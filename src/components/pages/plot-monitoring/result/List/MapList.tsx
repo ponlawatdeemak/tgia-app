ba5925f68ff06168b44b6483cf8f6c15de5ed092
Warutniy <warut.n@mapboss.co.th>
2024-09-02 18:33:13 +0700

    fix: update map layers

--- src/components/pages/plot-monitoring/result/List/MapList.tsx
+++ src/components/pages/plot-monitoring/result/List/MapList.tsx
@@ -2,7 +2,7 @@
 
 import MapView from '@/components/common/map/MapView'
 import classNames from 'classnames'
-import React, { useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import useSearchPlotMonitoring from '../Main/context'
 import useResponsive from '@/hook/responsive'
 import useAreaType from '@/store/area-type'
@@ -12,6 +12,58 @@ import { ResponseLanguage } from '@/api/interface'
 import { GetAreaSearchPlotDtoIn } from '@/api/plot-monitoring/dto-in.dto'
 import { useQuery } from '@tanstack/react-query'
 import service from '@/api'
+import useLayerStore from '@/components/common/map/store/map'
+import { MVTLayer } from '@deck.gl/geo-layers'
+import { Feature, Geometry } from 'geojson'
+import { apiAccessToken } from '@/api/core'
+import { AreaPlotTileColor, BoundaryTileColor, LineWidthColor, LossTypeColor, TotalTileColor } from '@/config/color'
+import { PickingInfo } from '@deck.gl/core'
+import InfoWindows from '../Map/InfoWindows'
+import { GetPositionSearchPlotDtoOut } from '@/api/plot-monitoring/dto-out.dto'
+
+type ProvincePropertiesType = {
+	layerName: string
+	provinceCode: number
+	provinceNameEn: string
+	provinceNameTh: string
+}
+
+type DistrictPropertiesType = {
+	layerName: string
+	districtCode: number
+	districtNameEn: string
+	districtNameTh: string
+	provinceCode: number
+	provinceNameEn: string
+	provinceNameTh: string
+}
+
+type SubDistrictPropertiesType = {
+	layerName: string
+	subDistrictCode: number
+	subDistrictNameEn: string
+	subDistrictNameTh: string
+	districtCode: number
+	districtNameEn: string
+	districtNameTh: string
+	provinceCode: number
+	provinceNameEn: string
+	provinceNameTh: string
+}
+
+type BoundaryLayerType = {
+	layerName: string
+	activity_id: number
+}
+
+type ClickInfo = {
+	x: number
+	y: number
+	area: GetPositionSearchPlotDtoOut
+}
+
+const SelectedLineWidth = 2
+const DefaultLineWidth = 0
 
 interface MapListProps {
 	areaDetail: string
@@ -22,8 +74,10 @@ const MapList: React.FC<MapListProps> = ({ areaDetail }) => {
 	const { isDesktop } = useResponsive()
 	const { areaType } = useAreaType()
 	const { areaUnit } = useAreaUnit()
+	const [clickInfo, setClickInfo] = useState<ClickInfo | null>(null)
 	const { t, i18n } = useTranslation(['default', 'field-loss'])
 	const language = i18n.language as keyof ResponseLanguage
+	const { layers, addLayer, setLayers } = useLayerStore()
 
 	const filterAreaSearchPlot = useMemo(() => {
 		const filter: GetAreaSearchPlotDtoIn = {
@@ -32,16 +86,340 @@ const MapList: React.FC<MapListProps> = ({ areaDetail }) => {
 			provinceCode: queryParams.provinceCode || undefined,
 			districtCode: queryParams.districtCode || undefined,
 			subDistrictCode: queryParams.subDistrictCode || undefined,
+			lossType: typeof queryParams.lossType === 'number' ? Number(queryParams.lossType) : undefined,
+			insuredType: typeof queryParams.insuredType === 'number' ? Number(queryParams.insuredType) : undefined,
+			publicStatus: typeof queryParams.publicStatus === 'number' ? Number(queryParams.publicStatus) : undefined,
+			riskType: queryParams.riskType || undefined,
+			riceType: queryParams.riceType || undefined,
+			detailType: queryParams.detailType || undefined,
 		}
 		return filter
 	}, [queryParams])
 
 	const { data: areaSearchPlot, isLoading: isAreaSearchPlotLoading } = useQuery({
 		queryKey: ['getAreaSearchPlot', filterAreaSearchPlot],
-		queryFn: () => service.plotMonitoring.getAreaSearchPlot(filterAreaSearchPlot),
+		queryFn: async () => {
+			const data = await service.plotMonitoring.getAreaSearchPlot(filterAreaSearchPlot)
+			setClickInfo(null)
+			return data
+		},
 	})
 
-	//console.log('areaSearchPlot', areaSearchPlot)
+	const areaSearchPlotId = useMemo(() => {
+		return areaSearchPlot?.data?.map((item) => item.activityId) || []
+	}, [areaSearchPlot])
+
+	const handlePositionClick = useCallback(
+		async (x: number, y: number, coordinate: number[], year: number) => {
+			try {
+				const response = await service.plotMonitoring.getPositionSearchPlot({
+					lat: coordinate[1],
+					lon: coordinate[0],
+					year: queryParams.year,
+				})
+				if (!response.data) {
+					throw new Error('Access Position failed!!')
+				}
+				setClickInfo({ x, y, area: response.data })
+			} catch (error) {
+				console.log('error: ', error)
+			}
+		},
+		[queryParams.year],
+	)
+
+	useEffect(() => {
+		if (!queryParams.provinceCode) {
+			setLayers([
+				new MVTLayer({
+					id: 'country',
+					name: 'country',
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: 'https://tileserver.cropinsurance-dev.thaicom.io/province/tiles.json',
+					filled: true,
+					lineWidthUnits: 'pixels',
+					getFillColor(d: Feature<Geometry, ProvincePropertiesType>) {
+						return BoundaryTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, ProvincePropertiesType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, ProvincePropertiesType>) {
+						return DefaultLineWidth
+					},
+					pickable: true,
+				}),
+			])
+		} else if (queryParams.provinceCode && !queryParams.districtCode) {
+			setLayers([
+				new MVTLayer({
+					id: 'province',
+					name: 'province',
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: 'https://tileserver.cropinsurance-dev.thaicom.io/province/tiles.json',
+					filled: true,
+					lineWidthUnits: 'pixels',
+					getFillColor(d: Feature<Geometry, ProvincePropertiesType>) {
+						return BoundaryTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, ProvincePropertiesType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, ProvincePropertiesType>) {
+						if (queryParams.provinceCode === d.properties.provinceCode) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.provinceCode,
+						getLineColor: queryParams.provinceCode,
+						getLineWidth: queryParams.provinceCode,
+					},
+				}),
+				new MVTLayer({
+					id: `boundary_$${queryParams.year}`,
+					name: `boundary_$${queryParams.year}`,
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: `https://tileserver.cropinsurance-dev.thaicom.io/boundary_${queryParams.year}/tiles.json`,
+					filled: true,
+					getFillColor(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return AreaPlotTileColor.rnr
+						}
+						return AreaPlotTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, BoundaryLayerType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.provinceCode,
+						getLineColor: queryParams.provinceCode,
+						getLineWidth: queryParams.provinceCode,
+					},
+					onClick: (info, event) => {
+						if (info.object) {
+							if (areaSearchPlotId.includes(info.object.properties.activity_id)) {
+								if (info.coordinate) {
+									handlePositionClick(info.x, info.y, info.coordinate, queryParams.year)
+								}
+							} else {
+								setClickInfo(null)
+							}
+						} else {
+							setClickInfo(null)
+						}
+					},
+				}),
+			])
+		} else if (queryParams.provinceCode && queryParams.districtCode && !queryParams.subDistrictCode) {
+			setLayers([
+				new MVTLayer({
+					id: 'district',
+					name: 'district',
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: 'https://tileserver.cropinsurance-dev.thaicom.io/district/tiles.json',
+					filled: true,
+					lineWidthUnits: 'pixels',
+					getFillColor(d: Feature<Geometry, DistrictPropertiesType>) {
+						return BoundaryTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, DistrictPropertiesType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, DistrictPropertiesType>) {
+						if (queryParams.districtCode === d.properties.districtCode) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.districtCode,
+						getLineColor: queryParams.districtCode,
+						getLineWidth: queryParams.districtCode,
+					},
+				}),
+				new MVTLayer({
+					id: `boundary_$${queryParams.year}`,
+					name: `boundary_$${queryParams.year}`,
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: `https://tileserver.cropinsurance-dev.thaicom.io/boundary_${queryParams.year}/tiles.json`,
+					filled: true,
+					getFillColor(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return AreaPlotTileColor.rnr
+						}
+						return AreaPlotTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, BoundaryLayerType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.districtCode,
+						getLineColor: queryParams.districtCode,
+						getLineWidth: queryParams.districtCode,
+					},
+					onClick: (info, event) => {
+						if (info.object) {
+							if (areaSearchPlotId.includes(info.object.properties.activity_id)) {
+								if (info.coordinate) {
+									handlePositionClick(info.x, info.y, info.coordinate, queryParams.year)
+								}
+							} else {
+								setClickInfo(null)
+							}
+						} else {
+							setClickInfo(null)
+						}
+					},
+				}),
+			])
+		} else if (queryParams.provinceCode && queryParams.districtCode && queryParams.subDistrictCode) {
+			setLayers([
+				new MVTLayer({
+					id: 'subDistrict',
+					name: 'subDistrict',
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: 'https://tileserver.cropinsurance-dev.thaicom.io/subdistrict/tiles.json',
+					filled: true,
+					lineWidthUnits: 'pixels',
+					getFillColor(d: Feature<Geometry, SubDistrictPropertiesType>) {
+						return BoundaryTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, SubDistrictPropertiesType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, SubDistrictPropertiesType>) {
+						if (queryParams.subDistrictCode === d.properties.subDistrictCode) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.subDistrictCode,
+						getLineColor: queryParams.subDistrictCode,
+						getLineWidth: queryParams.subDistrictCode,
+					},
+				}),
+				new MVTLayer({
+					id: `boundary_$${queryParams.year}`,
+					name: `boundary_$${queryParams.year}`,
+					loadOptions: {
+						fetch: {
+							headers: {
+								'content-type': 'application/json',
+								Authorization: `Bearer ${apiAccessToken}`,
+							},
+						},
+					},
+					data: `https://tileserver.cropinsurance-dev.thaicom.io/boundary_${queryParams.year}/tiles.json`,
+					filled: true,
+					getFillColor(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return AreaPlotTileColor.rnr
+						}
+						return AreaPlotTileColor.default
+					},
+					getLineColor(d: Feature<Geometry, BoundaryLayerType>) {
+						return LineWidthColor.default
+					},
+					getLineWidth(d: Feature<Geometry, BoundaryLayerType>) {
+						if (areaSearchPlotId.includes(d.properties.activity_id)) {
+							return SelectedLineWidth
+						}
+						return DefaultLineWidth
+					},
+					pickable: true,
+					updateTriggers: {
+						getFillColor: queryParams.subDistrictCode,
+						getLineColor: queryParams.subDistrictCode,
+						getLineWidth: queryParams.subDistrictCode,
+					},
+					onClick: (info, event) => {
+						if (info.object) {
+							if (areaSearchPlotId.includes(info.object.properties.activity_id)) {
+								if (info.coordinate) {
+									handlePositionClick(info.x, info.y, info.coordinate, queryParams.year)
+								}
+							} else {
+								setClickInfo(null)
+							}
+						} else {
+							setClickInfo(null)
+						}
+					},
+				}),
+			])
+		}
+	}, [
+		setLayers,
+		areaSearchPlot,
+		areaSearchPlotId,
+		handlePositionClick,
+		queryParams.provinceCode,
+		queryParams.districtCode,
+		queryParams.subDistrictCode,
+		queryParams.year,
+	])
 
 	return (
 		<div
@@ -49,6 +427,7 @@ const MapList: React.FC<MapListProps> = ({ areaDetail }) => {
 				'lg:hidden': areaDetail !== 'map',
 			})}
 		>
+			<InfoWindows clickInfo={clickInfo} setClickInfo={setClickInfo} />
 			<MapView />
 		</div>
 	)
