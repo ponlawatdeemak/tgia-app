5067c142d58656c7ff8799d0ab3c07afca456620
Ponlawat <ponlawat.d@mapboss.co.th>
2024-09-03 10:24:52 +0700

    add: file Data

---
+++ src/components/pages/others/glossary/data.ts
@@ -0,0 +1,12 @@
+export const test = [
+	{
+		word: {
+			th: 'พื้นที่ทบก.',
+			en: 'Farmmer registration area',
+		},
+		meaning: {
+			th: 'พื้นที่ปลูกข้าวที่ขึ้นทะเบียนเกษตรที่เข้าเกณฑ์ข้าวนาปี',
+			en: 'Registered agricultural land that qualifies for in- season rice cultivation',
+		},
+	},
+]
