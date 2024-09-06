9feb68116011dc21d44a84fbf0683021ade2e159
Warutniy <warut.n@mapboss.co.th>
2024-09-06 11:22:11 +0700

    fix: update plant and loss detail services

--- src/components/pages/plot-monitoring/result/id/Main/index.tsx
+++ src/components/pages/plot-monitoring/result/id/Main/index.tsx
@@ -1,33 +1,45 @@
 'use client'
 
 import React, { useState } from 'react'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import { Paper } from '@mui/material'
 import SummaryDetail from './SummaryDetail'
 import MapDetail from './MapDetail'
 import { useQuery } from '@tanstack/react-query'
 import service from '@/api'
+import PlantDetail from '../Detail/PlantDetail'
+import LossDetail from '../Detail/LossDetail'
+import useAreaType from '@/store/area-type'
 
 export const PlotMonitoringDetailMain = () => {
 	const pathname = usePathname()
+	const searchParams = useSearchParams()
+	const { areaType } = useAreaType()
 	const activityId = Number(pathname.split('/').pop())
+	const count = searchParams.get('count')
 
 	const [plotDetail, setPlotDetail] = useState<string>('plantDetail')
 
 	const { data: plantDetailData, isLoading: isPlantDetailDataLoading } = useQuery({
-		queryKey: ['getPlotActivityPlantDetail', activityId],
-		queryFn: () => service.plotMonitoring.getPlotActivityPlantDetail({ activityId }),
+		queryKey: ['getPlotActivityPlantDetail', activityId, count],
+		queryFn: () =>
+			service.plotMonitoring.getPlotActivityPlantDetail({ activityId, count: count ? parseInt(count) : 1 }),
 	})
 
 	const { data: lossDetailData, isLoading: isLossDetailDataLoading } = useQuery({
-		queryKey: ['getPlotActivityLossDetail', activityId],
-		queryFn: () => service.plotMonitoring.getPlotActivityLossDetail({ activityId }),
+		queryKey: ['getPlotActivityLossDetail', activityId, areaType, count],
+		queryFn: () =>
+			service.plotMonitoring.getPlotActivityLossDetail({
+				activityId,
+				registrationAreaType: areaType,
+				count: count ? parseInt(count) : 1,
+			}),
 	})
 
 	return (
 		<div className='flex flex-grow flex-col gap-4 overflow-auto'>
-			<Paper className='flex h-full overflow-hidden rounded-none lg:mx-4 lg:mb-4 lg:h-[calc(100vh-102px)] lg:rounded-lg'>
-				<div className='box-border flex w-full overflow-auto px-4 max-lg:flex-col max-lg:gap-3 max-lg:bg-gray-light max-lg:pb-4 lg:px-0'>
+			<Paper className='flex h-full overflow-hidden rounded-none lg:mx-4 lg:mb-4 lg:h-[calc(100vh-86px)] lg:rounded-lg'>
+				<div className='box-border flex w-full overflow-auto max-lg:flex-col max-lg:gap-3 max-lg:bg-white'>
 					<SummaryDetail
 						activityId={activityId}
 						plotDetail={plotDetail}
@@ -40,6 +52,13 @@ export const PlotMonitoringDetailMain = () => {
 						plotDetail={plotDetail}
 						lossType={lossDetailData?.data?.lossType}
 					/>
+					<div className='mx-4 lg:hidden'>
+						{plotDetail === 'plantDetail' ? (
+							<PlantDetail plantDetailData={plantDetailData?.data} />
+						) : (
+							<LossDetail lossDetailData={lossDetailData?.data} />
+						)}
+					</div>
 				</div>
 			</Paper>
 		</div>
