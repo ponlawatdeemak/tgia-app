6a37045484846397ffc2a84f247cc7007e4c19d8
Warutniy <warut.n@mapboss.co.th>
2024-10-16 13:47:14 +0700

    fix: UM issue#11

--- src/components/common/upload/UploadImage.tsx
+++ src/components/common/upload/UploadImage.tsx
@@ -27,8 +27,9 @@ const UploadImage: React.FC<UploadImageProps> = ({
 	const { t } = useTranslation()
 	const maxImageSize = 3 * 1024 * 1024
 	const [image, setImage] = useState<string | null>(null)
-	const errorMessage = formik.touched[name] && formik.errors[name]
+	//const errorMessage = formik.touched[name] && formik.errors[name]
 	const { i18n, tReady, ...uploadProps } = props
+	const [showInvalidFile, setShowInvalidFile] = useState(false)
 
 	useEffect(() => {
 		const formikValue = formik.values[name]
@@ -51,9 +52,11 @@ const UploadImage: React.FC<UploadImageProps> = ({
 			if (validImageTypes.includes(imageType) && imageSize <= maxImageSize) {
 				formik.setFieldValue(name, selectedImage)
 				setImage(URL.createObjectURL(selectedImage))
+				setShowInvalidFile(false)
 			} else {
 				formik.setFieldValue(name, null)
 				setImage(null)
+				setShowInvalidFile(true)
 			}
 		} else {
 			formik.setFieldValue(name, null)
@@ -95,7 +98,7 @@ const UploadImage: React.FC<UploadImageProps> = ({
 				)}
 			</>
 
-			<div className='[&_.Mui-disabled]:border-[#0000001f] [&_.Mui-disabled]:bg-transparent [&_.Mui-disabled]:text-[#00000042] [&_.Mui-disabled_.MuiButton-startIcon>svg]:text-[#00000042]'>
+			<div className='flex flex-col items-center [&_.Mui-disabled]:border-[#0000001f] [&_.Mui-disabled]:bg-transparent [&_.Mui-disabled]:text-[#00000042] [&_.Mui-disabled_.MuiButton-startIcon>svg]:text-[#00000042]'>
 				<Button
 					component='label'
 					role={undefined}
@@ -114,9 +117,10 @@ const UploadImage: React.FC<UploadImageProps> = ({
 						{...uploadProps}
 					/>
 				</Button>
+				{showInvalidFile && <FormHelperText error>{t('um:invalidFileSelected')}</FormHelperText>}
 			</div>
 			<p className='m-0 w-[123px] text-center text-sm text-[#7A7A7A]'>{t('common.conditionImg')}</p>
-			{typeof errorMessage === 'string' && <FormHelperText error>{errorMessage}</FormHelperText>}
+			{/* {typeof errorMessage === 'string' && <FormHelperText error>{errorMessage}</FormHelperText>} */}
 		</div>
 	)
 }
